7dd7810b49b6ac651e5ce0b30694a1abec527506
Taynara Silva <taynarassilva14@gmail.com>
2021-10-17 19:21:39 -0300

    SellerService e FindAll sincrono

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _contex;
+
+        public SellerService(SalesWebMVCContext contex)
+        {
+            _contex = contex;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _contex.Seller.ToList();
+        }
+    }
+}
